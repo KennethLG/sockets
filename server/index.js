699dad67c66d79893e63df75f269efeacb73bb81
KennethLG <orsonleiziaga@gmail.com>
2023-05-08 10:45:29 -0500

    Delete chat.js file, update server/index.js to use Vite for development and add alias for src folder in Vite config

--- server/index.js
+++ server/index.js
@@ -1,28 +1,43 @@
-const express = require('express');
-const http = require('http');
-const path = require('path');
-const socket = require('socket.io');
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const socket = require("socket.io");
+const { createServer } = require("vite");
 
-const app = express();
-const httpServer = http.createServer(app);
-const port = process.env.PORT || 3000;
-const io = new socket.Server(httpServer);
+async function start() {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const port = process.env.PORT || 3000;
+  const io = new socket.Server(httpServer);
 
-app.use(express.static(path.join(__dirname, '..', 'dist')));
+  const vite = await createServer({
+    root: path.resolve(__dirname, "..", "src"), // la carpeta donde está ubicado el proyecto de React
+    server: {
+      middlewareMode: true,
+      watch: {
+        usePolling: true,
+        interval: 100,
+      },
+    },
+  });
 
-app.get('*', (req, res) => {
+  app.use(vite.middlewares);
+
+  app.get("*", (req, res) => {
     res.send("Hello, world!");
-});
+  });
 
-io.on('connection', (socket) => {
-    socket.on('chat:message', (data) => {
-        io.sockets.emit('chat:message', data);
-    })
+  io.on("connection", (socket) => {
+    socket.on("chat:message", (data) => {
+      io.sockets.emit("chat:message", data);
+    });
 
-    socket.on('chat:typing', (data) => {
-        socket.broadcast.emit('chat:typing', data);
-    })
-})
+    socket.on("chat:typing", (data) => {
+      socket.broadcast.emit("chat:typing", data);
+    });
+  });
 
-httpServer.listen(port);
+  httpServer.listen(port);
+}
 
+start();
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import path from "path";
 
 export default defineConfig({
-  root: "public",
   plugins: [reactRefresh()],
   build: {
-    outDir: "../dist",
+    outDir: "../dist", // la carpeta donde se va a generar la compilación
+    emptyOutDir: true, // limpiar la carpeta antes de generar la compilación
   },
   server: {
-    proxy: {
-      "/socket.io": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        ws: true,
-      },
+    port: 3000, // el puerto en el que se va a ejecutar el servidor de desarrollo
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
